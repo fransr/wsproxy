215d06d36458dc6720a566c58034a74eba1bb334
Frans Rosén <frans@centra.com>
2024-02-19 21:52:55 +0100

    Initial commit

---
+++ wsproxy.mjs
@@ -0,0 +1,11 @@
+import { log } from './lib/log.mjs'
+import { cli } from './lib/cli.mjs'
+import { websocket } from './lib/websocket.mjs'
+import { httpServer } from './lib/http-server.mjs'
+
+await cli.argument_parse(process.argv);
+
+log.success('I AM GQLMAP WSPROXY');
+
+await websocket.start();
+httpServer.start();
